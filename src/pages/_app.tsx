78b4b3b18e5cbf466599a7131e06dfeaedd7246c
TheBinaryGuy <13674136+TheBinaryGuy@users.noreply.github.com>
2021-05-30 11:23:40 +0530

    :arrow_up: Upgraded packages.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 const App = ({ Component, pageProps }: AppProps) => {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>Next.js Starter!</title>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 };
 
 export default App;
